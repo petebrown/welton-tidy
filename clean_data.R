00fd00e313782df282da4a3d72c6b51d20a45b76
Pete Brown <15838790+petebrown@users.noreply.github.com>
2023-05-07 22:12:14 -0400

    first commit

---
+++ clean_data.R
@@ -0,0 +1,97 @@
+library(readxl)
+library(dplyr)
+library(readr)
+library(stringr)
+library(janitor)
+library(tidyr)
+
+df <- readxl::read_excel("./data/welton.xlsx", sheet = 2) %>%
+  janitor::clean_names()
+
+results <- df %>%
+  rename(
+    cup_round = r_6,
+    competition = comp,
+    venue = h_a,
+    gf = f,
+    ga = a,
+    scorers = goalscorers,
+    attendance = att,
+  ) %>%
+  mutate(
+    outcome = case_when(
+      gf > ga ~ "W",
+      gf == ga ~ "D",
+      gf < ga ~ "L"
+    ),
+    score = paste0(gf, "-", ga),
+    scorers = na_if(scorers, "x")
+  )
+
+results <- results %>%
+  group_by(season) %>%
+  arrange(date) %>%
+  mutate(
+    game_no = row_number()
+  ) %>%
+  ungroup()
+
+results <- results %>%
+  group_by(season, competition) %>%
+  arrange(date) %>%
+  mutate(
+    comp_game_no = row_number()
+  ) %>%
+  ungroup()
+
+results <- results %>%
+  select(
+    season,
+    date,
+    opponent,
+    venue,
+    score,
+    scorers,
+    competition,
+    cup_round,
+    game_no,
+    comp_game_no,
+    gf,
+    ga,
+    attendance,
+    manager,
+    referee
+  ) %>%
+  arrange(desc(date))
+
+scorers_list <- separate_wider_delim(
+  results,
+  scorers,
+  delim = ",",
+  names_sep = "",
+  too_few = "align_start",
+) %>%
+  select(
+    date,
+    starts_with("scorers")
+  ) %>%
+  mutate(
+    across(starts_with("scorers"), str_squish)
+  ) %>%
+  pivot_longer(
+    !date,
+    values_to = "scorer"
+  ) %>%
+  filter(
+    !is.na(scorer)
+  ) %>%
+  select(
+    date,
+    scorer
+  ) %>%
+  mutate(
+    no_of_goals = str_extract(
+      scorer, "\\d+"
+    ),
+    scorer2 = str_split(scorer, "[:space:]\\d+", n = 2)
+  )
