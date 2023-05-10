de8bf1c1b5770888fa37353e3a86bf8254fdbb83
petebrown <15838790+petebrown@users.noreply.github.com>
2023-05-10 17:10:32 -0400

    added streaks logic

--- clean_data.R
+++ clean_data.R
@@ -234,3 +234,60 @@ results %>%
   ggplot(aes(x = comp_game_no, y = ssn_pts)) +
   geom_line() +
   theme_bw()
+
+results %>%
+  # filter(season %in% input$season) %>%
+  # filter(season == "1964/65") %>%
+  arrange(season, game_no) %>%
+  group_by(season) %>%
+  mutate(
+    wins = ifelse(outcome == "W", 1, 0),
+    unbeaten = ifelse(outcome != "L", 1, 0),
+    losses = ifelse(outcome == "L", 1, 0),
+    winless = ifelse(outcome != "W", 1, 0),
+    draws = ifelse(outcome == "D", 1, 0),
+    cs = ifelse(ga == 0, 1, 0),
+    goalless = ifelse(gf == 0, 1, 0),
+    wins_cs = ifelse(outcome == "W" & ga == 0, 1, 0),
+    w_streak = ifelse(wins == 0, 0, sequence(rle(as.character(wins))$lengths)),
+    unbeaten_streak = ifelse(unbeaten == 0, 0, sequence(rle(as.character(unbeaten))$lengths)),
+    losing_streak = ifelse(losses == 0, 0, sequence(rle(as.character(losses))$lengths)),
+    winless_streak = ifelse(winless == 0, 0, sequence(rle(as.character(winless))$lengths)),
+    d_streak = ifelse(draws == 0, 0, sequence(rle(as.character(draws))$lengths)),
+    clean_sheets = ifelse(cs == 0, 0, sequence(rle(as.character(cs))$lengths)),
+    goalless_streak = ifelse(goalless == 0, 0, sequence(rle(as.character(goalless))$lengths)),
+  ) %>%
+  rename(Season = season) %>%
+  summarize(
+    "Wins" = max(w_streak),
+    "Unbeaten" = max(unbeaten_streak),
+    "Losses" = max(losing_streak),
+    "Winless" = max(winless_streak),
+    "Draws" = max(d_streak),
+    "Clean sheets" = max(clean_sheets),
+    "Firing blanks" = max(goalless_streak)
+  ) %>%
+  arrange(desc(`Firing blanks`))
+
+results %>%
+  filter(season == "2015/16") %>%
+  mutate(
+    gd = gf - ga
+  ) %>%
+  arrange(gd)
+
+results %>%
+  group_by(cup_round) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count)) %>%
+  print(n=100)
+
+results %>%
+  mutate(replay = case_when(
+    str_sub(cup_round, -1) == "R" ~ 1,
+    TRUE ~ 0
+  )) %>%
+  filter(game_type == "cup") %>%
+  arrange(competition, date) %>%
+  mutate(is_replay = ifelse()
+  )
